c9490fac6fda1c3c8267cc762d5e9dd506061d4e
lorraneidevic <lorraneidevic@gmail.com>
2018-02-18 19:01:11 -0300

    ejs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/produtos', function(req, res){
-    res.send('<html><body><h1>Listagem de Produtos</h1></body></html>')
+    console.log('listando');
+    res.render('produtos/lista');
 })
 
 app.listen(3000, function() {
-    console.log('servidor rodando')
+    console.log('servidor rodando');
 });
